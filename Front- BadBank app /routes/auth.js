ce979555d402414b21848816f34e8d2329a88928
Yasser Thabet <101029259+YasserSalahThabet@users.noreply.github.com>
2022-12-18 16:55:02 -0800

    Add files via upload

---
+++ Front- BadBank app /routes/auth.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { register, login, resetpassword, getUserInfo } = require('../controllers/auth');
+
+
+router.route("/register").post(register);
+
+router.route("/login").post(login);
+
+router.route("/getUserInfo").get( getUserInfo );
+module.exports = router;
---
+++ Front- BadBank app/server.js
@@ -0,0 +1,53 @@
+// server/index.js
+require('dotenv').config({ path: "./config.env" });
+const express = require("express");
+const connectDB = require('./config/db')
+const errorHandler = require('./middleware/error');
+const User = require("./models/Users");
+
+connectDB();
+
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static('public'))
+
+app.use(express.json());
+
+app.use('/api/auth', require("./routes/auth"));
+app.use('/api/private', require("./routes/private"));
+
+//error handle is last thing before server
+app.use(errorHandler);
+
+
+async function seedDatabase() {
+    const data = await User.findOne({ "username": "admin" }).exec();
+    if (data != null) {
+        // Data exists, no need to seed.
+        return;
+    }
+    const adminUser = new User({
+        username: "admin",
+        email: "admin@password.com",
+        password: "password",
+        isAdmin: true
+    });
+
+    await adminUser.save()
+}
+
+
+seedDatabase();
+
+
+const server = app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
+
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Logged Error: ${err}`);
+    server.close(() => process.exit(1));
+})
